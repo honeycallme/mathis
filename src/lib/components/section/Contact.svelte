893af59b24d77d7a6b7d94292ea94d7a1dac5c74
mathis <54747007+underhoney@users.noreply.github.com>
2024-03-06 18:03:23 +0100

    feature : added basic contact form

--- src/lib/components/section/Contact.svelte
+++ src/lib/components/section/Contact.svelte
@@ -1,26 +1,52 @@
 <script lang="ts">
-
 </script>
 
-<main class="relative bg-purple-200 h-[120dvh]" id="contact">
-    <img
-       src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F78.media.tumblr.com%2F50d43db43843a7dc2fbb93c660a1d0dc%2Ftumblr_ozar2dfRQI1rh3rypo1_1280.gif&f=1&nofb=1&ipt=4fc71b0b7a05822ead71374f474d2bf91bb1a30b8b24db40f978ee425e416c95&ipo=images"
-       alt="pixel background"
-       class="absolute inset-0 object-cover w-full h-full opacity-70"
-    />
+<main class="relative bg-purple-200 h-[110dvh]" id="contact">
+   <img
+      src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F78.media.tumblr.com%2F50d43db43843a7dc2fbb93c660a1d0dc%2Ftumblr_ozar2dfRQI1rh3rypo1_1280.gif&f=1&nofb=1&ipt=4fc71b0b7a05822ead71374f474d2bf91bb1a30b8b24db40f978ee425e416c95&ipo=images"
+      alt="pixel background"
+      class="absolute inset-0 object-cover w-full h-full opacity-70"
+   />
 
-
-    <div class="flex-col w-full h-full center">
-
+   <div class="flex-col w-full h-full center pt-[15%]">
       <div class="flex flex-col w-11/12">
-         <div class="grid h-20 card bg-base-300 rounded-box place-items-center">content</div> 
+         <div class="">
+            <h1 class="px-4 font-bold text-7xl adieu">Contact</h1>
+         </div>
 
          <!-- separator -->
-         <div class="divider"></div>
+         <div class="divider divider-neutral"></div>
 
-         <div class="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
-       </div>
+         <div class="flex px-4">
+            <div class="flex items-start flex-grow gap-8 text-xl">
+               <div>
+                  <h2 class="text-3xl adieu">Email</h2>
+               </div>
 
-    </div>
+               <div>
+                  <a href="mailto:mathisdoesstuff@gmail.com" class="link link-hover"
+                     >↗️ mathisdoesstuff@gmail.com</a
+                  >
+               </div>
+            </div>
 
- </main>
+            <div class="flex items-start flex-grow gap-8 text-xl">
+               <div>
+                  <h2 class="text-3xl adieu">Socials</h2>
+               </div>
+
+               <div class="flex flex-col">
+                  <a href="https://github.com/honeycallme" target="_blank" class="link link-hover">↗️ github</a>
+                  <a href="https://www.linkedin.com/in/mathisdoesstuff" target="_blank" class="link link-hover">↗️ linkedin</a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</main>
+
+<style>
+   .adieu {
+      font-family: "Adieu", sans-serif;
+   }
+</style>
